3aee5f49f1c04f94e655bad501e7365f0c38dd35
Rajiv Bose <rbose85@users.noreply.github.com>
2014-08-25 01:26:36 +0100

    add support for dynamic choice of fields in resp payload via qs params

--- backend/api/trades/serializers.py
+++ backend/api/trades/serializers.py
@@ -11,3 +11,16 @@ class TradeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Trade
         exclude = ("created", "modified")
+
+    def __init__(self, *args, **kwargs):
+        """custom initialisation of serializer to support dynamic field list"""
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs["context"].pop("fields", None)
+
+        # Instantiate the superclass normally
+        super(TradeSerializer, self).__init__(*args, **kwargs)
+
+        if fields:
+            # Drop fields not specified in the `fields` argument.
+            for field_name in (set(self.fields.keys()) - set(fields)):
+                self.fields.pop(field_name)
--- backend/api/trades/views.py
+++ backend/api/trades/views.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
 from django.core.exceptions import ValidationError
+from djangorestframework_camel_case.util import camel_to_underscore
 from rest_framework import serializers, viewsets
 
 from trades.models import Trade
-
 from .serializers import TradeSerializer
 
 
@@ -17,20 +17,34 @@ class TradeViewSet(viewsets.ReadOnlyModelViewSet):
 
     @staticmethod
     def __iso_string_to_datetime(iso_string):
-        """iterate over list of strings and build datetime obj from each iso"""
+        """build datetime obj from iso8601 string"""
         try:
             dt = serializers.DateTimeField().from_native(iso_string)
         except ValidationError:
             return None
         return dt
 
+    def __reduce_serializer_field_set(self, field_names):
+        """iterate over list of field names and return preferred super-set"""
+        default_fields = list(self.get_serializer_class().get_fields().keys())
+        for fn in field_names:
+            if fn not in default_fields:
+                return None
+        return field_names
+
     def __extract_from_querystring(self, param_name, param_cls):
         """search querystring for parameter by name and return as instance"""
         param = self.request.QUERY_PARAMS.get(param_name)
         if param is None:
-            return None
+            return param
+
         if param_cls.__name__ == 'datetime':
             return self.__iso_string_to_datetime(iso_string=param)
+
+        if param_cls.__name__ == 'list':
+            underscored = [camel_to_underscore(x) for x in param.split(',')]
+            return self.__reduce_serializer_field_set(field_names=underscored)
+
         return None
 
     def get_queryset(self):
@@ -46,3 +60,11 @@ class TradeViewSet(viewsets.ReadOnlyModelViewSet):
             queryset = queryset.filter(time_stamp__lte=finish)
 
         return queryset
+
+    def get_serializer_context(self):
+        """Optionally set queryset fields based on url query parameters."""
+        context = super(TradeViewSet, self).get_serializer_context()
+
+        preferreds = self.__extract_from_querystring('preferred_columns', list)
+        context['fields'] = preferreds
+        return context
